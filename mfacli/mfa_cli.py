5f17a82b44a3c06a9a65797c5113ab11c4c43aa6
Thiago Almeida <thiagoalmeidasa@gmail.com>
2019-07-29 15:01:34 +0200

    Adding yapf ignore block in order to fix lint issue

--- mfacli/mfa_cli.py
+++ mfacli/mfa_cli.py
@@ -28,13 +28,15 @@ except ImportError:
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
+# yapf: disable
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version='1.0.0')
 def main():
     """CLI tool for generating one-time passwords."""
-    pass
 
 
+# yapf: enable
+@click.group(context_settings=CONTEXT_SETTINGS)
 @main.command()
 @click.option('-kf',
               '--keyfile',
