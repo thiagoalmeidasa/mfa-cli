b9529bca8b65fae34cc83c2728391eb9db13cae0
Thiago Almeida <thiagoalmeidasa@gmail.com>
2021-05-08 21:13:23 +0200

    Bump dependency versions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ version = re.search('^__version__\s*=\s*"(.*)"',
                     open('mfacli/mfa_cli.py').read(), re.M).group(1)
 
 with open("README.md", "rb") as f:
-    long_descr = f.read().decode("utf-8")
+    LONG_DESCR = f.read().decode("utf-8")
 
 setup(
     name="mfa-cli",
@@ -16,13 +16,13 @@ setup(
     entry_points={"console_scripts": ['mfa-cli = mfacli.mfa_cli:main']},
     version=version,
     description='CLI tool for generating one-time passwords.',
-    long_description=long_descr,
+    long_description=LONG_DESCR,
     author='Thiago Almeida',
     author_email='thiagoalmeidasa@gmail.com',
     url='https://github.com/thiagoalmeidasa/mfa-cli',
     install_requires=[
-        'pyotp==2.2.7', 'pyyaml==5.4', 'click==7.0', 'pyaescrypt==0.4.3',
-        'pyperclip==1.7.0'
+        'pyotp==2.6.0', 'pyyaml==5.4', 'click==7.1', 'pyaescrypt==5.0.0',
+        'pyperclip==1.8.2'
     ],
     tests_require=['pylint'],
 )
